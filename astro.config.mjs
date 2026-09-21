27b99a4db1ffadf491862b7abc93d6b400a37592
DanOrm94 <142906041+DanOrm94@users.noreply.github.com>
2026-09-21 10:01:32 +0100

    fix: use static Astro output for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   integrations: [tailwind()],
   site: 'https://mbautos.co.uk',
   build: { format: 'directory' }
